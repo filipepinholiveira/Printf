c7ef348d18668980df1f09960d812517414bbe77
Filipe Andre Pinho De Oliveira <fpinho-d@c1r14s6.42porto.com>
2023-01-06 19:01:23 +0000

    Trabalho inicial

--- ft_printf.c
+++ ft_printf.c
@@ -1,16 +1,51 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fpinho-d <fpinho-d@student.42porto.com>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/06 18:54:02 by fpinho-d          #+#    #+#             */
-/*   Updated: 2023/01/06 18:54:29 by fpinho-d         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <stdarg.h>
 
-int	main()
+
+
+* -> 4 bytes 
+
+(void *) -> char*
+	|
+	. -> int *
+
+#include <stdlib.h>
+
+malloc => void *
+
+while (format_line != '\0')
 {
+	int count = 0;
 
+	if (*format_line == '%' && next_symbol_exist())
+	{
+		if (*(format_line + 1) == 'd' || == 'i')
+			count += print_decimal(va_arg(list, int));
+		else if (*(format_line + 1) == 's')
+			count += ft_putstr(va_arg(list, char *));
+		else if ('x')
+
+		else if ('c')
+
+		else if ('p')
+			1) covert int to heximal & save that hec in string
+			
+
+	}
+	return count;
+}
+
+int main(void)
+{
+	if ('-')
+		ft_printf("%-s", heximal_line);
+	printf("%#X\n", "-424242");
+	42 -> 'x' -> 2a
+	42 -> 'X' -> 2A
+	"%#X" -> 0X2A
+	"%#x" -> 0x2a
+	" +#"
+	
+	printf("%.5d\n", -424242);
+	return (0);
 }
